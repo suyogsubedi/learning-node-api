bf54ad4457271e7b456e21d9b9ecec64c9c5660b
Suyog Subedi <subedisuyog555@gmail.com>
2021-01-30 10:13:02 +0545

    Created Schema

---
+++ src/models/crmModels.js
@@ -0,0 +1,29 @@
+// Schema defines the rules related to what the database can accept
+
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema({
+  firsName: {
+    type: String,
+    required: "Enter a first name",
+  },
+  lastName: {
+    type: String,
+    required: "Enter a last name",
+  },
+  email: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
