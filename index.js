39d5892e7ce1f9e331a36097991ff173a41f5689
Suyog Subedi <subedisuyog555@gmail.com>
2021-01-30 10:03:26 +0545

    Connection with database

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import express, { response } from "express";
 import routes from "./src/routes/crmRoutes";
+// import mongoose
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
 
-// We are passsing the app in our route finction which is in crmRoutes.js
+// Mongoose connection
+// The following line suggests that we will be waiting for the result while connecting
+mongoose.Promise = global.Promise;
+
+// The following line helps to connect with the db
+mongoose.connect("mongodb://localhost/CRMdb", {
+  // They are going to depreciate this code block in the future so you need to write this
+
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Body Parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// We are passsing the app in our route function which is in crmRoutes.js
 routes(app);
 
 app.get("/", (req, res) => {
